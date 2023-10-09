bcb6a734762ce3b03a1a7c2464f3521e6caa3a12
Alisa Tyutyunkova <94295868+ATyutyunkova@users.noreply.github.com>
2023-10-09 22:21:00 +0500

    add serviceCollectionExtensions

--- WebServer/HhVacancyAnalyzerServer.Api/Program.cs
+++ WebServer/HhVacancyAnalyzerServer.Api/Program.cs
@@ -1,5 +1,4 @@
-using HhVacancyAnalyzerServer.Provider.Interfaces;
-using HhVacancyAnalyzerServer.Provider.Services;
+using HhVacancyAnalyzerServer.Provider.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +6,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddTransient<IItRolesHandlerService, ItRolesHandlerService>();
+builder.Services.AddServices();
 
 var app = builder.Build();
 
---
+++ WebServer/HhVacancyAnalyzerServer.Provider/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using HhVacancyAnalyzerServer.Provider.Interfaces;
+using HhVacancyAnalyzerServer.Provider.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HhVacancyAnalyzerServer.Provider.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    // TODO: think about name for method later
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IItRolesHandlerService, ItRolesHandlerService>();
+    }
+}
