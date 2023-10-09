df8ad2894394aec229f54a5f456b6acb441e722d
Alisa Tyutyunkova <94295868+ATyutyunkova@users.noreply.github.com>
2023-10-09 22:04:43 +0500

    add getting it roles

---
+++ WebServer/HhVacancyAnalyzerServer.Api/Program.cs
@@ -0,0 +1,24 @@
+using HhVacancyAnalyzerServer.Provider.Interfaces;
+using HhVacancyAnalyzerServer.Provider.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IItRolesHandlerService, ItRolesHandlerService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
---
+++ WebServer/HhVacancyAnalyzerServer.Provider/Models/ItRole.cs
@@ -0,0 +1,7 @@
+﻿namespace HhVacancyAnalyzerServer.Provider.Models;
+
+public class ItRole
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
